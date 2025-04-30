3ecb5cf0ec0424243484e7aea152b19f541f2aa8
sara.f.silva.simao <sara.f.silva.simao@WINAPXHTDDWISEE>
2025-04-30 16:54:22 -0300

    incluindo projeto .net

---
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, Mundo!");
+        Console.ReadLine(); // espera você apertar Enter antes de fechar
+    }
+}
